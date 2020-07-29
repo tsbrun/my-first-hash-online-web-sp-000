51ef654829e4757bd6966c25c732fa340864fe8b
Anya Brun <tsbrun@icloud.com>
2020-07-29 15:45:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+    "Captain Janeway" => "Starship Voyager"
+    "Captain Archer" => "Starship Enterprise"
+    "Captain Sisko" => "Deep Space Nine"
+  }
 
 end
 
