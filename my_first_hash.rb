07bf71c315cba5eec599ca2e6e070815cd96798c
Anya Brun <tsbrun@icloud.com>
2020-07-29 15:51:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,13 +39,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = 3
+  shipping_manifest["muskets"] = 3
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
